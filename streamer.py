1a19266f107c83af680de9f1247aa4ec5823c030
rounak <rounakdatta12@gmail.com>
2019-02-20 22:57:45 +0530

    no urllib; using requests
    options to encode it / take it raw

--- streamer.py
+++ streamer.py
@@ -1,6 +1,21 @@
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from subprocess import Popen, PIPE
+import requests
+import base64
+
+def getChunkedData(audioURL):
+
+    r = requests.get(audioURL, stream=True)
+    for chunk in r.iter_content(chunk_size=1024):
+
+        if encoded:
+            print(base64.b64encode(chunk))
+        else:
+            print(chunk)
+
+        print()
+
+# change this to False if you want the decoded \x00-like format
+encoded = True
 
 videoID = '8dsctieMibU'
 process = Popen(['youtube-dl', '--get-url', '-f', '140', 'https://www.youtube.com/watch?v=' + videoID], stdout=PIPE, stderr=PIPE)
@@ -8,8 +23,4 @@ stdout, stderr = process.communicate()
 payloadURL = stdout.decode("utf-8") 
 print(payloadURL)
 
-http = urllib3.PoolManager()
-r = http.request('GET', payloadURL, preload_content=False)
-
-for chunk in r.stream(32):
-    print(chunk)
+getChunkedData(payloadURL)
